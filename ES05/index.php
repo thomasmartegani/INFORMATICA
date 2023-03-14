46d386fed655ae1ada3bba891859f40006b4282c
thomasmartegani <117729578+thomasmartegani@users.noreply.github.com>
2023-03-14 12:44:19 +0100

    Create index.php

---
+++ ES05/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>BENVENUTO</title>
+</head>
+<body>
+<h1>WEBAPP</h1>
+<br>
+<a href="login.php">
+Effettua il login!
+</a> 
+<br>
+<a href="riservata.php">
+Vai alla pagina riservata!
+</body>
+</html>
